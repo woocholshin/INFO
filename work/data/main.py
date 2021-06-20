f2dba685fc11320139863663e409ce8319a596c3
woocholshin <33920920+woocholshin@users.noreply.github.com>
2021-06-21 00:33:30 +0900

    Add files via upload

--- work/data/main.py
+++ work/data/main.py
@@ -20,4 +20,26 @@ for i in range(len(x)):
 
 print(node.getRMSE(csv.KOSPI, csv.ln_KOSPI))
 
+#========================================================
+# test variables...
+p = 3
+q = 2
+l = m = n = p
+lr = 0.1
+
+y = np.array([10, 20, 30])
+e = np.array([0.1,0.2])
+
+# ML
+W0, W1, W2, W3, W4 = node.initWeightMatrix(p, q, l, m, n)
+
+o1, o2, o3, o4 = node.partialFoward(y, W0, W2, W3, W4)
+
+print("[TARGET VALUE]" , y)
+print("[OUTPUT]" , o4)
+print("[OUTPUT1]" , o1)
+res = node.getresidual(y, o4)
+print(res)
+
+
 
