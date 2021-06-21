1ed94f4638bbc10df2333697791ba0deee817adc
woocholshin <33920920+woocholshin@users.noreply.github.com>
2021-06-21 19:54:09 +0900

    Add files via upload

--- work/data/main.py
+++ work/data/main.py
@@ -25,21 +25,58 @@ print(node.getRMSE(csv.KOSPI, csv.ln_KOSPI))
 p = 3
 q = 2
 l = m = n = p
-lr = 0.1
+lr = 0.5
 
 y = np.array([10, 20, 30])
 e = np.array([0.1,0.2])
+
+total_input = np.concatenate((y, e))
+
 
 # ML
 W0, W1, W2, W3, W4 = node.initWeightMatrix(p, q, l, m, n)
 
 o1, o2, o3, o4 = node.partialFoward(y, W0, W2, W3, W4)
 
-print("[TARGET VALUE]" , y)
-print("[OUTPUT]" , o4)
-print("[OUTPUT1]" , o1)
-res = node.getresidual(y, o4)
-print(res)
+W1_new, W2_new, W3_new, W4_new = node.fullBackward(y, W1, W2, W3, W4, o1, o2, o3, o4, lr)
+print("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
+print("[1.TEST in progress...]\n")
+print(W1)
+print(W1_new)
+print("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
+
+o1, o2, o3, o4 = node.partialFoward(y, W0, W2, W3, W4)
+
+W1_new, W2_new, W3_new, W4_new = node.fullBackward(y, W1, W2, W3, W4, o1, o2, o3, o4, lr)
+print("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
+print("[TEST in progress...]\n")
+print(W1_new)
+print("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
+
+o1, o2, o3, o4 = node.fullFoward(total_input, W1_new, W2_new, W3_new, W4_new)
+
+W1_new, W2_new, W3_new, W4_new = fullBackward(y, W1, W2, W3, W4, o1, o2, o3, o4, lr)
+
+
+
+
+
+
+
+
+
+
+
+o1, o2, o3, o4 = node.fullFoward(total_input, W1_new, W2_new, W3_new, W4_new)
+
+W1_new, W2_new, W3_new, W4_new = fullBackward(y, W1, W2, W3, W4, o1, o2, o3, o4, lr)
+
+
+
+
+
+
+
 
 
 
